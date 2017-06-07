a85b0193271a9c822c5df11acc586fcfa81f71ec
songzheng45 <songzheng45@126.com>
2017-06-07 20:51:44 +0800

    Add some files

---
+++ WebAPI/FirstWebProject/FirstWebProject/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Http;
+
+namespace FirstWebProject.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(DateTime.Now);
+        }
+    }
+}
