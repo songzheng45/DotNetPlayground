4f604c4c5d81b1753fbdbd2e6cdefebd75a59f18
songxingzheng <songxingzheng@chanyikeji.com>
2017-06-07 20:56:31 +0800

    add existing project

---
+++ WebAPI/WebAPIStreamToClient/WebAPIStreamToClient/Controllers/VideoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using WebAPIStreamToClient.Models;
+
+namespace WebAPIStreamToClient.Controllers
+{
+    public class VideoController : ApiController
+    {
+        public HttpResponseMessage Get(string filename, string ext)
+        {
+            VideoStream videoStream = new VideoStream(filename, ext);
+
+            var response = Request.CreateResponse();
+            Action<Stream, HttpContent, TransportContext> writeAction = videoStream.WriteToStream;
+            response.Content = new PushStreamContent(writeAction, new MediaTypeHeaderValue($"video/{ext}"));
+            response.Content.Headers.ContentLength = videoStream.FileSize;
+
+            return response;
+        }
+    }
+}
---
+++ WebAPI/WebAPIStreamToClient/WebAPIStreamToClient/Models/VideoStream.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Hosting;
+
+namespace WebAPIStreamToClient.Models
+{
+    public class VideoStream
+    {
+        private readonly string _filename;
+
+        public long FileSize { get; set; }
+
+        public VideoStream(string filename, string ext)
+        {
+            _filename = HostingEnvironment.MapPath($"~/downloads/{filename}.{ext}");
+        }
+
+        public async void WriteToStream(Stream outputStream, HttpContent content, TransportContext context)
+        {
+            try
+            {
+                var buffer = new byte[65536];
+                using (var video = File.Open(_filename, FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    FileSize = video.Length;
+
+                    var bytesRead = buffer.Length;
+
+                    while (video.CanRead)
+                    {
+                        bytesRead = video.Read(buffer, 0, buffer.Length);
+                        await outputStream.WriteAsync(buffer, 0, bytesRead);
+                    }
+                }
+            }
+            catch (HttpException e)
+            {
+                return;
+            }
+            catch (IOException e)
+            {
+                return;
+            }
+            finally
+            {
+                outputStream.Close();
+            }
+        }
+    }
+}
