807eb79083859c6473fa1cb991f8291f417e80ad
songxingzheng <songxingzheng@126.com>
2017-06-19 19:13:39 +0800

    update

---
+++ NetCore/UbuntuDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace UbuntuDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            Console.WriteLine(".NET Core App is runing...");
+
+            string filename = "log.txt";
+            if (!File.Exists(filename))
+            {
+                File.Create(filename);
+            }
+
+
+
+            for (int index = 0; index < 300; index++)
+            {
+                Thread.Sleep(1000);
+
+                string msg = $"北京时间：{DateTime.Now}!";
+
+                using (StreamWriter writer = new StreamWriter(File.Open(filename, FileMode.Append)))
+                {
+                    writer.WriteLine(msg);
+                    writer.Flush();
+                }
+            }
+        }
+    }
+}
