87c6a1d76f3ca998d5a432b6754c3e123429bcdc
songzheng45 <songzheng45@126.com>
2017-07-11 23:37:41 +0800

    update redis demo and chat app demo

---
+++ MyChatApp/MyChatApp/Models/ErrorHandlingPipelineModule.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyChatApp.Models
+{
+    public class ErrorHandlingPipelineModule : HubPipelineModule
+    {
+        protected override void OnIncomingError(ExceptionContext exceptionContext, IHubIncomingInvokerContext invokerContext)
+        {
+            // TODO 
+            base.OnIncomingError(exceptionContext, invokerContext);
+        }
+    }
+}
---
+++ RedisDemo/RedisDemo01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
+namespace RedisDemo01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Connect 或 ConnectAsync
+            // ConnectionMultiplexer 实现了 IDisposable 接口，当不再需要时会自动释放，并且为了简洁，不必使用 using 包含。
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+
+            // 如果涉及 master/slave (主从)设置, 尽管将所有节点包含进来, Redis 会自动标志出 master.
+            // ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("server1:6379,server2:6379");
+
+
+            IDatabase db = redis.GetDatabase();
+            db.StringSet("key1", DateTime.Now.ToString());
+            string value = db.StringGet("key1");
+            Console.WriteLine($"value={value}");
+
+
+            var hs = new HashSet<string>();
+            hs.Add(Guid.NewGuid().ToString());
+            hs.Add(Guid.NewGuid().ToString());
+            value = JsonConvert.SerializeObject(hs);
+            db.StringSet("www", value);
+
+            var jsonValue = db.StringGet("www");
+            Console.WriteLine(jsonValue);
+            var hsValue = JsonConvert.DeserializeObject<HashSet<string>>(jsonValue);
+
+            foreach (var item in hsValue)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
