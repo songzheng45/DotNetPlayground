31b3726ccc70dfeaaf13416656801365c700b7e7
songzheng45 <songzheng45@126.com>
2017-08-15 18:32:58 +0800

    add RoleMangager

--- IdentityDemo/IdentityDemo01/App_Start/IdentityConfig.cs
+++ IdentityDemo/IdentityDemo01/App_Start/IdentityConfig.cs
@@ -14,15 +14,16 @@ namespace Users
             // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
 
             // 说明: CreatePerOwinContext() 方法为每一个请求创建新的 AppIdentityDbContext 和 AppUserManager实例
-            app.CreatePerOwinContext<AppIdentityDbContext>(AppIdentityDbContext.Create);
+            app.CreatePerOwinContext(AppIdentityDbContext.Create);
             app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.CreatePerOwinContext<AppRoleManager>(AppRoleManager.Create);
 
             // 告诉 Identity 如何使用cookie去标识已验证过的用户
             app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions
             {
                 // 验证类型. 
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                
+
                 // 如果没有通过验证则跳转到这个URL
                 LoginPath = new PathString("/Account/Login")
             });
