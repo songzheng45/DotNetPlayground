dc84d8a56d29b0008b9c7f38520caa42c6edf5c7
robin <robin@ds.com>
2018-10-31 14:55:36 +0800

    new

---
+++ CSharp/SpanSample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SpanSample
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TryReadOnlySpan();
+        }
+
+        static void TryReadOnlySpan()
+        {
+            string str = "Hello, World!";
+            string worldStr
+                = str.Substring(startIndex: 7, length: 5);    // 分配内存
+
+            ReadOnlySpan<char> worldSpan
+                = str.AsSpan().Slice(start: 7, length: 5);
+
+            Debug.Assert('W' == worldSpan[0]);
+
+            // 编译不通过：
+            // 不能给属性 ReadOnlySpan<char>.this[int] 赋值，因为它是只读变量
+            //worldSpan[0] = 'a';     
+        }
+
+        static void TryStackalloc()
+        {
+            //Span<byte> bytes;
+            //unsafe
+            //{
+            //    byte* tmp = stackalloc byte[length];
+            //    bytes = new Span<byte>(tmp, length);
+            //}
+
+            //
+            // 现在可以使用关键字 stackalloc 来分配内存,而不必使用 unsafe 关键字
+            //
+            Span<byte> bytes = stackalloc byte[3];
+
+        }
+    }
+}
---
+++ StripUTF8Bom/ConsoleApp/Program.cs
@@ -0,0 +1,128 @@
+﻿
+using System;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp
+{
+    // 将 UTF-8 Bom 转换为 UTF-8 的几种方法
+    class Program
+    {
+        static string fileName = "TextUTF8WithBom.txt";
+
+        static void Main(string[] args)
+        {
+            byte[] buffer = File.ReadAllBytes(fileName);
+            PrintIfHasBom(buffer);
+            PrintBytesLength(buffer);
+
+            Print();
+
+            byte[] newBuffer;
+            Print("方法1:");
+            newBuffer = Method1(buffer);
+            PrintIfHasBom(newBuffer);
+            PrintBytesLength(newBuffer);
+
+            Print();
+
+            Print("方法2:");
+            newBuffer = Method2(buffer);
+            PrintIfHasBom(newBuffer);
+            PrintBytesLength(newBuffer);
+
+            Print();
+
+            Print("方法3:");
+            newBuffer = Method3(buffer);
+            PrintIfHasBom(newBuffer);
+            PrintBytesLength(newBuffer);
+        }
+
+        static byte[] Method3(byte[] buffer)
+        {
+            if (buffer == null) return null;
+
+            string str;
+
+            if (buffer.Length <= 3) return buffer;
+
+            byte[] bomBuffer = new byte[] { 0xef, 0xbb, 0xbf };
+
+            if (buffer[0] == bomBuffer[0]
+                && buffer[1] == bomBuffer[1]
+                && buffer[2] == bomBuffer[2])
+            {
+                str = new UTF8Encoding(false).GetString(buffer, 3, buffer.Length - 3);
+            }
+            else
+            {
+                str = Encoding.UTF8.GetString(buffer);
+            }
+
+            byte[] newBuffer = Encoding.UTF8.GetBytes(str);
+            return newBuffer;
+        }
+
+        private static byte[] Method2(byte[] buffer)
+        {
+            string str = Encoding.UTF8.GetString(buffer)?.Trim('\uFEFF', '\u200B');
+
+            byte[] newBuffer = Encoding.UTF8.GetBytes(str);
+            return newBuffer;
+        }
+
+        static byte[] Method1(byte[] buffer)
+        {
+            byte[] withBom = { 0xef, 0xbb, 0xbf, 0x41 };
+
+            string viaStreamReader;
+            using (StreamReader reader = new StreamReader
+                   (new MemoryStream(buffer), Encoding.UTF8))
+            {
+                viaStreamReader = reader.ReadToEnd();
+            }
+
+            byte[] newBuffer = Encoding.UTF8.GetBytes(viaStreamReader);
+            return newBuffer;
+        }
+
+        static bool ContainBom(byte[] buffer)
+        {
+            if (buffer == null || buffer.Length <= 3) return false;
+
+            byte[] bomBuffer = new byte[] { 0xef, 0xbb, 0xbf };
+
+            if (buffer[0] == bomBuffer[0]
+                && buffer[1] == bomBuffer[1]
+                && buffer[2] == bomBuffer[2])
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static void PrintBytesLength(byte[] buffer)
+        {
+            Print("字节数组长度: " + buffer.Length);
+        }
+
+        static void PrintIfHasBom(byte[] buffer)
+        {
+            bool containBom = ContainBom(buffer);
+            Print($"是否包含 BOM : {containBom}");
+        }
+
+        static void Print(string str = null)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                System.Console.WriteLine();
+            }
+            else
+            {
+                System.Console.WriteLine(str);
+            }
+        }
+    }
+}
